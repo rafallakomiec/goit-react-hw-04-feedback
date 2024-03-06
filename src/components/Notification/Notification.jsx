211fba6ef01f1da42df34c39f5d0ab1cc2531800
Rafal Lakomiec <rafal.lakomiec@gmail.com>
2024-03-06 18:20:24 +0100

    Refactor Notification comp

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -1,17 +1,10 @@
-import { Component } from 'react';
 import propTypes from 'prop-types';
 
-class Notification extends Component {
-  static propTypes = {
-    message: propTypes.string.isRequired
-  };
+export default Notification = props => {
+  const { message } = props;
+  return <p>{message}</p>;
+};
 
-  render() {
-    const { message } = this.props;
-    return (
-        <p>{message}</p>
-    );
-  }
-}
-
-export default Notification;
+Notification.propTypes = {
+  message: propTypes.string.isRequired,
+};
