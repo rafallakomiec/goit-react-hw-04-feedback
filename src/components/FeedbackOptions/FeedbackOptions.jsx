96e679df16cc30040439296741e856c60a75811b
Rafal Lakomiec <rafal.lakomiec@gmail.com>
2024-03-06 18:22:38 +0100

    Refactor FeedbackOptions comp

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,20 +1,15 @@
-import { Component } from 'react';
 import propTypes from 'prop-types';
 
-class FeedbackOptions extends Component {
-    static propTypes = {
-        option: propTypes.oneOf(['Good', 'Bad', 'Neutral']).isRequired,
-        onLeaveFeedback: propTypes.func.isRequired
-    };
-
-    render() {
-        const {option, onLeaveFeedback} = this.props;
-        return (
-          <button type="button" onClick={onLeaveFeedback}>
-            {option}
-          </button>
-        );
-    }
+export default FeedbackOptions = (props) => {
+  const {option, onLeaveFeedback} = props;
+  return (
+    <button type="button" onClick={onLeaveFeedback}>
+      {option}
+    </button>
+  );
 }
 
-export default FeedbackOptions;
+FeedbackOptions.propTypes = {
+  option: propTypes.oneOf(['Good', 'Bad', 'Neutral']).isRequired,
+  onLeaveFeedback: propTypes.func.isRequired
+};
