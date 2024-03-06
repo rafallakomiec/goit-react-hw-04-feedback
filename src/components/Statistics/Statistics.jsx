556f41162198fae5346c35f54a2c22662961fe8a
Rafal Lakomiec <rafal.lakomiec@gmail.com>
2024-03-06 18:15:28 +0100

    Refactor Statistics comp

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,38 +1,35 @@
 import { Component } from 'react';
 import propTypes from 'prop-types';
 
-class Statistics extends Component {
-  static defaultProps = {
+export default Statistics = (
+  props = {
     good: 0,
     neutral: 0,
     bad: 0,
     total: 0,
     positivePercentage: 0,
-  };
-  static propTypes = {
-    good: propTypes.number.isRequired,
-    neutral: propTypes.number.isRequired,
-    bad: propTypes.number.isRequired,
-    total: propTypes.number.isRequired,
-    positivePercentage: propTypes.number.isRequired,
-  };
-
-    render() {
-        const { good, neutral, bad, total, positivePercentage } = this.props;
-        
-        return (
-          <section>
-            <h2>Statistics</h2>
-            <ul>
-              <li>Good: {good}</li>
-              <li>Neutral: {neutral}</li>
-              <li>Bad: {bad}</li>
-              <li>Total: {total}</li>
-              <li>Positive feedback: {positivePercentage}%</li>
-            </ul>
-          </section>
-        );
   }
-}
+) => {
+  const { good, neutral, bad, total, positivePercentage } = props;
 
-export default Statistics;
+  return (
+    <section>
+      <h2>Statistics</h2>
+      <ul>
+        <li>Good: {good}</li>
+        <li>Neutral: {neutral}</li>
+        <li>Bad: {bad}</li>
+        <li>Total: {total}</li>
+        <li>Positive feedback: {positivePercentage}%</li>
+      </ul>
+    </section>
+  );
+};
+
+Statistics.propTypes = {
+  good: propTypes.number.isRequired,
+  neutral: propTypes.number.isRequired,
+  bad: propTypes.number.isRequired,
+  total: propTypes.number.isRequired,
+  positivePercentage: propTypes.number.isRequired,
+};
